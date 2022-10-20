26d1cc0082387f5b7bb7d65dabad2d78175bd80b
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2022-10-20 12:48:58 +0300

    Delete pyyton2 directory

--- pyyton2/yl7.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Sisesta number: "))
-if (num % 2) == 0:
-   print("{0} on paaris".format(num))
-else:
-   print("{0} on paaritu".format(num))
--- pyyton2/yl8.py
+++
@@ -1,2 +0,0 @@
-from truths import Truths
-print Truths (['A', 'B', 'C'], ['A AND (B OR C)', '(A  B) OR NOT(C AND A)'], ints=False)
