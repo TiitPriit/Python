9213d4c75a45d79ba5f106a518fb3d752c6a344f
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2022-10-20 12:44:03 +0300

    Add files via upload

---
+++ pyyton2/yl7.py
@@ -0,0 +1,5 @@
+num = int(input("Sisesta number: "))
+if (num % 2) == 0:
+   print("{0} on paaris".format(num))
+else:
+   print("{0} on paaritu".format(num))
---
+++ pyyton2/yl8.py
@@ -0,0 +1,2 @@
+from truths import Truths
+print Truths (['A', 'B', 'C'], ['A AND (B OR C)', '(A  B) OR NOT(C AND A)'], ints=False)
