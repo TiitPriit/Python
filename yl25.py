aaffa14f9d217f2d9f37dd6b796fadd2d097a445
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-01-16 14:09:05 +0200

    Update yl25.py

--- yl25.py
+++ yl25.py
@@ -3,6 +3,12 @@ me = {
     "last_name": "Kallas",
     "birth_year": "2006",
     "place_of_living": "Leisi",
-    "dessert": "Ice_cream",
-    
+    "dessert": "candy",
 }
+
+print(me.get("place_of_living"))
+print(me["place_of_living"])
+
+me["dessert"] = "ice_cream"
+
+print(me)
