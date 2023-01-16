e5342b940b1d3b6bf50e1b8646b850a3242a5f08
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-01-16 13:30:55 +0200

    yl24 korda

--- yl24.py
+++ yl24.py
@@ -1,18 +1,23 @@
-def upc(a):
-    a = str(a)
-    counter = 0
-    checkerneg = 0
-    checkerpos = 0
-    if len(a) < 11:
-        a = ("0")*(11-len(a))+a
-    for n in a:
-        if counter % 2 == 0:
-            checkerneg += int(n)
-        else:
-            checkerpos += int(n)
-        counter += 1
-    M = ((checkerneg*3)+checkerpos) % 10
-    if M != 0:
-        return 10 - M
-    else:
-        return 0
+def upc_check_digit(upc):
+    check_sum = 0
+    for i in range (1, 12, 2):
+        check_sum += int(upc[i-1])
+        
+    check_sum *= 3
+
+    for i in range (2, 11, 2):
+        check_sum += int(upc[i-1])
+
+    reminder = check_sum % 10
+
+    if ( reminder != 0 ):
+        check_sum = 10 - reminder
+    else :
+        check_sum = 0
+
+
+    print(check_sum)
+
+
+upc_check_digit("042100005264")
+upc_check_digit("036000291452")
