e4c13941ac8eb091ff7fd232fea3a707b73f2309
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-01-23 09:24:05 +0200

    Update yl26.py

--- yl26.py
+++ yl26.py
@@ -1,10 +1,27 @@
-def calculate_bonus(sales_dict):
-    bonus_dict = {}
-    for employee, sales in sales_dict.items():
-        if sales > 10000:
-            bonus = (sales - 10000) * 0.1
-            bonus_dict[employee] = bonus
-    return bonus_dict
+def calculate_commission(revenue, expenses):
+    commission = {}
+    for salesperson in revenue:
+        commission[salesperson] = 0
+        for i in range(len(revenue[salesperson])):
+            profit = revenue[salesperson][i] - expenses[salesperson][i]
+            if profit > 0:
+                commission[salesperson] += profit * 0.062
+    return commission
 
-sales = {'John': 15000, 'Sara': 12000, 'Bob': 9000}
-print(calculate_bonus(sales)) # {'John': 500.0, 'Sara': 200.0}
+revenue = {"Frank": [120, 243], "Jane": [145, 265]}
+expenses = {"Frank": [130, 143], "Jane": [59, 198]}
+print(calculate_commission(revenue, expenses))
+
+def calculate_commission(revenue, expenses):
+    commission = {}
+    for salesperson in revenue:
+        commission[salesperson] = 0
+        for i in range(len(revenue[salesperson])):
+            profit = revenue[salesperson][i] - expenses[salesperson][i]
+            if profit > 0:
+                commission[salesperson] += profit * 0.062
+    return commission
+
+revenue = {"Johnver": [190, 325, 682, 829], "Vanston": [140, 19, 14, 140], "Danbree": [1926, 293, 852, 609], "Vansey": [14, 1491, 56, 120], "Mundyke": [143, 162, 659, 87]}
+expenses = {"Johnver": [120, 300, 50, 67], "Vanston": [65, 10, 299, 254], "Danbree": [890, 23, 1290, 89], "Vansey": [54, 802, 12, 129], "Mundyke": [430, 235, 145, 76]}
+print(calculate_commission(revenue, expenses))
