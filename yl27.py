b78f2fd16a4b4d6538538aa30f09285c6b99a29b
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-01-26 11:38:42 +0200

    Update yl27.py

--- yl27.py
+++ yl27.py
@@ -2,7 +2,7 @@
 #pip on Pythoni paigaldusprogramm, mis võimaldab teil Pythoni raamatukogusid paigaldada ja haldada. See on tavaliselt Pythoni arendajatele kättesaadav tööriist, mis võimaldab teil oma rakendustes kasutada teiste arendajate poolt loodud raamatukogusid.
 #PRAW (Python Reddit API Wrapper) on Pythoni raamatukogu, mis võimaldab teil lihtsalt ja efektiivselt kasutada Redditi API-d. See võimaldab teil luua rakendusi, mis kasutavad Redditi andmeid, näiteks automaatset postituste ja kommentaaride lisamist, andmete kraapimist jne.
 
-import praw
+"""import praw
 
 reddit = praw.Reddit(client_id='YOUR_CLIENT_ID', 
                      client_secret='YOUR_CLIENT_SECRET', 
@@ -11,4 +11,13 @@ reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
                      user_agent='YOUR_USER_AGENT')
 
 for submission in reddit.user.me().submissions.new(limit=10):
-    print(submission.title)
+    print(submission.title)"""
+
+
+import praw
+
+reddit = praw.Reddit(
+    client_id="my client id",
+    client_secret="my client secret",
+    user_agent="my user agent",
+)
