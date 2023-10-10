b3c80198fcc1e1aff0acb7fbb70db3e371c7e145
Oskar.Kallas <112690706+TiitPriit@users.noreply.github.com>
2023-10-10 13:08:41 +0300

    main

---
+++ Proovivoor/eio.py
@@ -0,0 +1,17 @@
+
+A, B, C, D = map(float, input().split())
+
+keep_warm_energy = C * 60  
+
+cooling_energy = 0
+time_to_cool = 0
+
+while time_to_cool < 60:
+    cooling_energy += D * (60 - time_to_cool)  
+    time_to_cool += B  
+    if time_to_cool < 60:
+        cooling_energy += (60 - time_to_cool) * A  
+        time_to_cool += 60  
+
+print(round(keep_warm_energy, 2))
+print(round(cooling_energy, 2))
